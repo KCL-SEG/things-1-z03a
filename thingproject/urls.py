883825488917437e52d57d99fdfec93d8b0e1878
Zahra Amaan <k21011879@kcl.ac.uk>
2022-12-02 10:50:12 +0000

    things 1

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from things import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("projectApp.urls")),
+    path('', views.mainPage, name='mainPage')
 ]
 
 
--- things/models.py
+++ things/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Thing(models.Model):
+    name = models.CharField(
+        max_length = 30,
+        unique = True,
+        blank = False
+    )
+    description = models.CharField(
+        max_length = 120,
+        unique = False,
+        blank = True
+    )
+    quantity  = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ]
+    )
